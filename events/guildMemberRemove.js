ac85eb6a37e01abb9c85e6b6ed380aa11f704447
Unknown <youssefelshemi@gmail.com>
2018-12-09 16:02:50 +0000

    welcom

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,12 +1,12 @@
 const Event = require('../util/Event');
 
-class GuildMemberAdd extends Event {
+class GuildMemberRemove extends Event {
   constructor(...args) {
     super(...args);
   }
 
   run(member) {
-    console.log(`Member ${member.displayName} joined ${member.guild.name}`)
+    console.log(`Member ${member.displayName} left ${member.guild.name}`)
     if (member.guild.id === '311483608697274379') {
       const role = member.guild.roles.get('358670733951369217');
       member.roles.add(role);
@@ -14,13 +14,13 @@ class GuildMemberAdd extends Event {
 
       const embed = new Discord.MessageEmbed()
         .setAuthor(`${member.user.username}`, member.user.avatarURL())
-        .setDescription(`Say hello to ${member.user.username}, everyone! We all need a warm welcome sometimes :D`)
+        .setDescription(`Say goodbye to ${member.user.username}, everyone! We will all miss you :(`)
         .setColor(member.guild.member(member.client.user.id).roles.highest.color || 0x00AE86)
         .setTimestamp()
-        .setFooter('User has joined: ');
+        .setFooter('User has left: ');
       channel.send(embed);
     }
   }
 }
 
-module.exports = GuildMemberAdd;
+module.exports = GuildMemberRemove;
